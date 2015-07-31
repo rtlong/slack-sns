c72ebc696eaf3e57da68e44129e99d9c0e84f0ea
Ryan Taylor Long <ryan@rtlong.com>
2015-07-30 19:34:35 -0700

    Refactor
    [no story]

--- test/handlers.js
+++ test/handlers.js
@@ -1,10 +1,82 @@
 import { assert } from 'chai'
 
-var cloudformationMessage = 'StackId=\'arn:aws:cloudformation:us-east-1:394453120661:stack/InternalToolsTest/1ec91b30-35bf-11e5-bc32-5001b34a4a0a\'\nTimestamp=\'2015-07-29T23:41:28.277Z\'\nEventId=\'55351f20-364b-11e5-8a00-5001a7da7436\'\nLogicalResourceId=\'InternalToolsTest\'\nNamespace=\'394453120661\'\nPhysicalResourceId=\'arn:aws:cloudformation:us-east-1:394453120661:stack/InternalToolsTest/1ec91b30-35bf-11e5-bc32-5001b34a4a0a\'\nPrincipalId=\'AIDAIDOWE7O3X7JLV2LRY\'\nResourceProperties=\'null\'\nResourceStatus=\'DELETE_COMPLETE\'\nResourceStatusReason=\'\'\nResourceType=\'AWS::CloudFormation::Stack\'\nStackName=\'InternalToolsTest\'\n'
+import {
+  parseIncoming,
+  parseAsLineDelimitedKVPairs
+} from '../lib/handlers'
 
-describe('foo', () => {
-  it('foos', () => {
-    assert.equal(cloudformationMessage, cloudformationMessage)
-    assert.equal(cloudformationMessage, true)
+var messages = {
+  cloudformation: {
+    real: 'StackId=\'arn:aws:cloudformation:us-east-1:394453120661:stack/InternalToolsTest/1ec91b30-35bf-11e5-bc32-5001b34a4a0a\'\nTimestamp=\'2015-07-29T23:41:28.277Z\'\nEventId=\'55351f20-364b-11e5-8a00-5001a7da7436\'\nLogicalResourceId=\'InternalToolsTest\'\nNamespace=\'394453120661\'\nPhysicalResourceId=\'arn:aws:cloudformation:us-east-1:394453120661:stack/InternalToolsTest/1ec91b30-35bf-11e5-bc32-5001b34a4a0a\'\nPrincipalId=\'AIDAIDOWE7O3X7JLV2LRY\'\nResourceProperties=\'null\'\nResourceStatus=\'DELETE_COMPLETE\'\nResourceStatusReason=\'\'\nResourceType=\'AWS::CloudFormation::Stack\'\nStackName=\'InternalToolsTest\'\n',
+  },
+  lineDelimited: {
+    foo: "foo='bar'\nbiz='fizz bang'\n",
+  },
+  json: {
+    foo: JSON.stringify({ foo: 'bar', biz: 'fizz bang' }),
+  },
+  plain: {
+    foo: 'this is plain text and should not be parsed',
+  },
+}
+
+describe('handlers', () => {
+  describe('parseIncoming', () => {
+    it('parses line-delimited stuff correctly', () => {
+      let msg = messages.lineDelimited.foo
+      let parsed = parseIncoming(msg)
+      assert.equal(parsed[0], 'line-delimited')
+      assert.deepEqual(parsed[1], {
+        foo: 'bar',
+        biz: 'fizz bang',
+      })
+    })
+
+    it('parses JSON stuff correctly', () => {
+      let msg = messages.json.foo
+      let parsed = parseIncoming(msg)
+      assert.equal(parsed[0], 'json')
+      assert.deepEqual(parsed[1], {
+        foo: 'bar',
+        biz: 'fizz bang',
+      })
+    })
+
+    it('falls back to plain text', () => {
+      let msg = messages.plain.foo
+      let parsed = parseIncoming(msg)
+      assert.equal(parsed[0], 'text')
+      assert.equal(parsed[1], messages.plain.foo)
+    })
+  })
+
+  describe('parseAsLineDelimitedKVPairs', () => {
+    it('parses line-delimited stuff correctly', () => {
+      let msg = messages.lineDelimited.foo
+      let parsed = parseAsLineDelimitedKVPairs(msg)
+      assert.deepEqual(parsed, {
+        foo: 'bar',
+        biz: 'fizz bang',
+      })
+    })
+
+    it('parses line-delimited stuff correctly', () => {
+      let msg = messages.cloudformation.real
+      let parsed = parseAsLineDelimitedKVPairs(msg)
+      assert.deepEqual(parsed, {
+        StackId: 'arn:aws:cloudformation:us-east-1:394453120661:stack/InternalToolsTest/1ec91b30-35bf-11e5-bc32-5001b34a4a0a',
+        Timestamp: '2015-07-29T23:41:28.277Z',
+        EventId: '55351f20-364b-11e5-8a00-5001a7da7436',
+        LogicalResourceId: 'InternalToolsTest',
+        Namespace: '394453120661',
+        PhysicalResourceId: 'arn:aws:cloudformation:us-east-1:394453120661:stack/InternalToolsTest/1ec91b30-35bf-11e5-bc32-5001b34a4a0a',
+        PrincipalId: 'AIDAIDOWE7O3X7JLV2LRY',
+        ResourceProperties: 'null',
+        ResourceStatus: 'DELETE_COMPLETE',
+        ResourceStatusReason: '',
+        ResourceType: 'AWS::CloudFormation::Stack',
+        StackName: 'InternalToolsTest',
+      })
+    })
   })
 })
